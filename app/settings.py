4c70d0c7e35711979f3b3e6e363f1cc7644e8f26
vmigunov <you@example.com>
2025-04-16 22:19:24 +0300

    refactored settings

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlite_db_name: str = "pomodoro.sqlite"
+    DB_HOST: str = "db"
+    DB_PORT: int = 5432
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "123"
+    DB_DRIVER: str = "postgresql+asyncpg"
+    DB_NAME: str = "pomodoro"
+    CACHE_HOST: str = "0.0.0.0"
+    CACHE_PORT: int = 6379
+    CACHE_DB: int = 0
 
 
 settings = Settings()

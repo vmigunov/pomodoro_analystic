0869115ca07898592147b9f90bdabc773f80493c
vmigunov <you@example.com>
2025-06-10 03:24:21 +0300

    fix: deleted print db_url

--- app/settings.py
+++ app/settings.py
@@ -27,16 +27,12 @@ class Settings(BaseSettings):
 
     @property
     def db_url(self):
-        print(
-            f"{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-        )
         return f"{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     @property
     def google_redirect_url(self) -> str:
         return f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={self.GOOGLE_CLIENT_ID}&redirect_uri={self.GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline"
-    
-    
+
     @property
     def yandex_redirect_url(self) -> str:
         return f"https://oauth.yandex.ru/authorize?response_type=code&client_id={self.YANDEX_CLIENT_ID}&force_confirm=yes"
